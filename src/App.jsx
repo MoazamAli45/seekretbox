706bcc36140b3508ad776e3a1ac21b1fc4b355dc
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2024-04-21 15:33:41 +0500

    Add chat icon to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
 import "./App.css";
 import Agenda from "./components/Agenda";
 import Contact from "./components/Contact";
@@ -5,7 +6,6 @@ import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Marketing from "./components/Marketing";
 import Navbar from "./components/Navbar";
-
 function App() {
   return (
     <>
@@ -15,6 +15,10 @@ function App() {
       <Agenda />
       <Contact />
       <Footer />
+      {/*  Chat Icon */}
+      <span className="flex items-center justify-center bg-[#373B4D] w-[65px] h-[65px] rounded-full shadow-sm shadow-white/50 fixed right-4 bottom-5">
+        <IoChatbubbleEllipsesOutline className="text-white text-[35px]" />
+      </span>
     </>
   );
 }
